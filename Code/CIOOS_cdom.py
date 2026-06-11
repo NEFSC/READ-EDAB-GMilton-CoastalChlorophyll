f5d4c28af320b8a97cad86b8059f0aa121901020
GMilton2 <gianna.milton@noaa.gov>
2026-06-11 13:07:46 -0400

    Added comments

--- Code/CIOOS_cdom.py
+++ Code/CIOOS_cdom.py
@@ -1,120 +1,93 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Apr 16 13:50:32 2026
-CIOOS_cdom
+Created on Wed Apr 15 10:37:34 2026
+CIOOS_chl
+CIOOS chlorophyll data, found by searching 'chlorophyll' on their erddap and only keeping non-flourecent (in-vivo) chlorophyll values
+
 @author: gianna.milton
 """
-
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import xarray as xr
-import cartopy
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
-from shapely import geometry
 import geopandas as gpd
-from datetime import datetime
-import os
-from os.path import join, getsize
-from matplotlib import ticker
-import datetime as dt
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import cmocean as cm
-import cmocean.cm as cmo
-from shapely.ops import unary_union
-import matplotlib.gridspec as gridspec
-from pyproj import Transformer
-from erddapy import ERDDAP
-import time
-import matplotlib.ticker as mticker
+import requests
+import xarray as xr 
 
-keast  = {"min_time": "2000-01-01T00:00:00Z","max_time": "2025-12-12T00:00:00Z"}
 
-server = "http://data.cioospacific.ca/erddap"
-e = ERDDAP(server=server, protocol="tabledap")
-url = e.get_search_url(search_for="cdom", response="csv",**keast) 
-#no cdom in gulf, alaska, or hawaii
-dfs = pd.read_csv(url)
-
-dataframes_east_cdom = {}
-dataframes_west_cdom = {}
-
-reg_names_cdom = ['dataframes_east_cdom','dataframes_west_cdom']
-df_names = ['dfs_east','dfs_west']
-
-for idx2 in range(len(reg_names_cdom)):
-    print('region '+reg_names_cdom[idx2])
-    for idx, row in globals()[df_names[idx2]].iterrows():
-        dataset_id = row['Dataset ID']
-        e.dataset_id = dataset_id
-        e.variables = ['time', 'latitude', 'longitude', 'cdom ','cdom_qc_agg','z']  #only append these columns
-        try:
-            print(f"Loading {dataset_id}...")
-            df_data = e.to_pandas(index_col="time (UTC)")
-            globals()[reg_names_cdom[idx2]][dataset_id] = df_data
-            #add these to dataframe to ensure metadata is recorded
-            globals()[reg_names_cdom[idx2]][dataset_id]['Dataset ID'] = dataset_id #add a column for dataset id
-            globals()[reg_names_cdom[idx2]][dataset_id]['source'] = 'IOOS' #add source column
-            globals()[reg_names_cdom[idx2]][dataset_id]['Institution'] = row['Institution'] #add institude column
-            globals()[reg_names_cdom[idx2]][dataset_id]['url'] = row['Background Info'] #add link to sensor website
-            globals()[reg_names_cdom[idx2]][dataset_id]['experiment'] = row['Title'] #add project to dataset
-            time.sleep(1)  # small delay to avoid hammering the server
-        except Exception as ex:
-            print(f"Failed to load {dataset_id}") #{ex}
-            
-#rename for easier concatination
-for idx2 in range(len(reg_names_cdom)):    
-    for dsid, df in globals()[reg_names_cdom[idx2]].items():
-        df = df.reset_index()
-        df = df.rename(columns={'latitude (degrees_north)': 'lat', 'longitude (degrees_east)': 'lon', 'cdom (microg.L-1)': 'cdom',
-            'cdom (1e-9)': 'cdom', 'z (m)': 'depth', 'time (UTC)':'datetime'})
-        df.datetime=pd.to_datetime(df.datetime.astype(str),format='mixed')
-        df = df[df['cdom_qc_agg'] != 3] #remove all 3s from dataframe i.e suspect points
-        df = df[df['cdom_qc_agg'] != 4] #remove all 4s from dataframe i.e. failed points 
-        
-        globals()[reg_names_cdom[idx2]][dsid] = df  # update the dict
-
-#reduced to 1 day average withing top 10 meters 
-for idx2 in range(len(reg_names_cdom)):    
-    for dsid, df in globals()[reg_names_cdom[idx2]].items(): 
-        df=df[(df['depth']>=-10) & (df['depth']<=10)] #only within top 10 meters
-        df['date'] = df['datetime'].dt.date
-        df = df.drop(columns='datetime')
-        df=df.groupby(['date','Dataset ID','source','Institution','url','experiment']).mean() #groupby date and take average
-        globals()[reg_names_cdom[idx2]][dsid] = df.reset_index()
-        
-
-dataframes_east = pd.concat(dataframes_east_cdom.values(), ignore_index=True)
-dataframes_west = pd.concat(dataframes_west_cdom.values(), ignore_index=True)
-
-dfs=[dataframes_east,dataframes_west]
-ioos_cdom = pd.concat(dfs).reset_index(drop=True)
-ioos_cdom=ioos_cdom[['date', 'lat', 'lon', 'cdom','depth','Dataset ID', 'source', 'Institution','url', 'experiment',]]
-ioos_cdom['date'] = pd.to_datetime(ioos_cdom['date'])
-ioos_cdom = ioos_cdom.loc[ioos_cdom['date'] > '2000-01-01'] 
-ioos_cdom = ioos_cdom.dropna(subset=['cdom'])
-ioos_cdom['depth'] = ioos_cdom['depth'].abs() #make sure depth is not positive
- 
-ioos_cdom.to_excel('ioos_cdom_na.xlsx', index = False)
+url='https://data.cioospacific.ca/erddap/tabledap/IYS_NISKIN_chl_phaeo.nc?cruise%2Cstation%2Ctime%2Clatitude%2Clongitude%2Cdepth%2Cchlorophyll_a&time%3E=2019-03-07T00%3A00%3A00Z&time%3C=2019-03-14T15%3A59%3A00Z'
+url = requests.get(url, verify=False).content #load in data from their erddap
+da = xr.open_dataset(url)
+da1= da.to_dataframe() #format dataset into dataframe
+da1 = da1.rename(columns={'time': 'datetime','latitude':'lat','longitude':'lon','chlorophyll_a':'chl'}) #standardize column names 
+#add metadata
+da1['experiment'] = da.project 
+da1['investigators']=da.publisher 
+da1['affiliations']=da.institution
+da1['DOI_url']= da.infoUrl
+da1['HPLC'] = 1 #after researching project, not HPLC methods used
+counts_series = da1[['depth','datetime','lat','lon']].value_counts() #count how many unique cast,depth, datetime, lat, and lons there are
+da1['triplicate'] = 1 #based on inpsecting counts_series, no triplicates
 
 
 
+url='https://data.cioospacific.ca/erddap/tabledap/IOS_BOT_Profiles.nc?mission_id%2Cscientist%2Cproject%2Cagency%2Cplatform%2Cinstrument_type%2Cinstrument_model%2Clatitude%2Clongitude%2Ctime%2Cdepth%2CCPHLFLP1&time%3E=2000-01-01&time%3C=2024-08-30T03%3A17%3A44Z'
+url = requests.get(url, verify=False).content
+da = xr.open_dataset(url)
+da2= da.to_dataframe() #convert to dataframe
+da2 = da2.dropna(subset=['CPHLFLP1']) #if no chlorophyll data in row, drop
+da2 = da2.rename(columns={'scientist': 'investigators','project':'experiment','agency':'affiliations','platform':'station',
+                          'latitude':'lat','longitude':'lon','time':'datetime','CPHLFLP1':'chl'})  #standardize column name
+#triplicate flag
+counts_series = da2[['depth','datetime','lat','lon']].value_counts() #count how many unique cast,depth, datetime, lat, and lons there are
+counts_df = counts_series.reset_index(name='freq_uniq')
+da2 = pd.merge(da2, counts_df, on=['depth','datetime','lat','lon'], how='left') #add frequency column to original dataframe4
+
+#sometimes, triplicate specific times are recorded (ex: 3:00, 3:05, 3:10 ), so also check for unique datehour entries
+da2['date_hour'] = da2['datetime'].dt.strftime('%Y-%m-%d %H')
+counts_series = da2[['depth','date_hour','lat','lon']].value_counts() #count how many unique datehour, lat, and lons there are
+counts_df = counts_series.reset_index(name='freq_hour')
+da2 = pd.merge(da2, counts_df, on=['depth','date_hour','lat','lon'], how='left') #add frequency column to original dataframe
+da2['triplicate'] = 1 #assume bad unless otherwise said
+da2.loc[da2['freq_uniq'] == 3, 'triplicate'] = 0 #if there was a unique datetime, lat, and lon that happened 3 times, triplicate
+da2.loc[(da2['freq_uniq'] == 1) &(da2['freq_hour'] == 3), 'triplicate'] = 0 #if 1 unique datetime but 3 unique date hours, assume triplicate
+
+da2=da2[['investigators', 'experiment', 'affiliations', 'station', 'lat', 'lon', 'datetime', 'depth', 'chl', 'triplicate']]
+da2['HPLC'] = 1 #based on researching project, no HPLC methods used
+da2['DOI_url']='https://data.cioospacific.ca/erddap/tabledap/IOS_BOT_Profiles'
+da2=da2.loc[da2['depth']<=150].reset_index(drop=True)#only subset to top 150 m
+da2 = da2[da2['chl'] >= 0] #remove any negative chlorophyll values
 
 
+url='https://cioosatlantic.ca/erddap/tabledap/bio_atlantic_zone_monitoring_program_bottle_bbmp.nc?mission_descriptor%2Cinstitute%2Cstation%2Clatitude%2Clongitude%2Ctime%2Cdepth%2CChlorophyll_A%2CHPLC_chlorophyll_a&time%3E=2000-01-01&time%3C=2024-12-31T13%3A19%3A00Z'
+url = requests.get(url, verify=False).content
+da = xr.open_dataset(url)
+da3= da.to_dataframe()
+da3=da3.dropna(subset=['Chlorophyll_A', 'HPLC_chlorophyll_a'], how='all') #this project records both non-HPLC and HPLC data. remove any rows that have both types empty
+da3 = da3.rename(columns={'mission_descriptor': 'cruise','institute':'affiliations','HPLC_chlorophyll_a':'chl_a',
+                          'latitude':'lat','longitude':'lon','time':'datetime','Chlorophyll_A':'chl'}) 
+
+#some chl and chl_a values are shifted down, creating duplicated datetime and sample information instead of being coincidental. so need to re aling them 
+metadata_cols = ['cruise', 'affiliations', 'station', 'lat', 'lon', 'datetime', 'depth'] #designate columns to keep 
+da3 = da3.groupby(metadata_cols, as_index=False).first() #group by unique metadata sample information
+#add metadata information 
+da3['experiment'] = da.program 
+da3['investigators']=da.chief_scientist 
+da3['affiliations']=da.institution
+da3['DOI_url']= da.publisher_url
+#HPLC
+da3['HPLC'] = 0 #initiate all columns as HPLC= true, only empty values will be 1 
+da3.loc[da3['chl_a'].isna(), 'HPLC'] = 1 #if chl_a not recorded, turn to false
+counts_series = da3[['depth','datetime','lat','lon']].value_counts() #count how many unique cast,depth, datetime, lat, and lons there are
+da3['triplicate'] = 1 #based on inpsecting counts_series, no triplicates
+
+#combine the datasets into single dataframe
+df_combined = pd.concat([da1, da2,da3], ignore_index=True)
+
+#subset to the shapefile 
+shp = gpd.read_file(r'C:\Users\gianna.milton\Documents\Python\Shapefiles\combined_coastline.shp')
+gdf = gpd.GeoDataFrame(df_combined, geometry=gpd.points_from_xy(df_combined.lon, df_combined.lat), crs="EPSG:4269")
+gdf = gdf.to_crs(shp.crs)
+df_combined = gpd.sjoin(gdf, shp, how="inner", predicate="within")
+columns_to_drop = ['geometry', 'index_right', 'merge_id']
+df_combined = df_combined.drop(columns=columns_to_drop)
+df_combined= df_combined.reset_index(drop=True)
 
 
-
-
-
-
-
-
-
-
-
-
+df_combined.to_excel('CIOOS_chl.xlsx')
--- Code/CalCOFI_chl.py
+++ Code/CalCOFI_chl.py
@@ -12,26 +12,26 @@ warnings.filterwarnings('ignore', category=pd.errors.SettingWithCopyWarning)
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 #load in bottle data from CalCOFI website
-Calcofi_btl = pd.read_excel(r'C:\Users\gianna.milton\Documents\Python\CalCOFI_BEUTI\CalCOFI_Database_194903-202105_csv_16October2023\CalCOFI_Database_194903-202105_csv_16October2023\CalCOFI_Database_194903-202105_csv_16October2023\194903-202105_Bottle.xlsx')#chl files
+Calcofi_btl = pd.read_excel(r'CalCOFI_Database_194903-202105_csv_16October2023\CalCOFI_Database_194903-202105_csv_16October2023\CalCOFI_Database_194903-202105_csv_16October2023\194903-202105_Bottle.xlsx')#chl files
 Calcofi_btl = Calcofi_btl[['Cst_Cnt','Sta_ID', 'Depthm',  'ChlorA', 'Chlqua','R_Depth']] #reduce to only needed columns
 
 #since bottle data has no datetime, lat, or lon, need to load in CalCOFI meta data and match up to bottle data
-Calcofi_cast = pd.read_excel(r'C:\Users\gianna.milton\Documents\Python\CalCOFI_BEUTI\CalCOFI_Database_194903-202105_csv_16October2023\CalCOFI_Database_194903-202105_csv_16October2023\CalCOFI_Database_194903-202105_csv_16October2023\194903-202105_Cast.xlsx')
+Calcofi_cast = pd.read_excel(r'CalCOFI_Database_194903-202105_csv_16October2023\CalCOFI_Database_194903-202105_csv_16October2023\CalCOFI_Database_194903-202105_csv_16October2023\194903-202105_Cast.xlsx')
 Calcofi_cast = Calcofi_cast[['Cst_Cnt', 'Cruise', 'Sta_ID', 'Sta_Code', 'Date', 'Time', 'Lat_Dec', 'Lon_Dec', 'Data_Type']]
 
 #create datetime column
 Calcofi_cast['Date'] = Calcofi_cast['Date'].astype(str)
 Calcofi_cast['Time'] = Calcofi_cast['Time'].astype(str)
-Calcofi_cast['Date'] = Calcofi_cast['Date'].str.slice(0, 10)  # Keeps only 'YYYY-MM-DD'
-datetime_str = Calcofi_cast['Date'] + ' ' + Calcofi_cast['Time']
-Calcofi_cast['datetime'] = pd.to_datetime(datetime_str, errors='coerce')
+Calcofi_cast['Date'] = Calcofi_cast['Date'].str.slice(0, 10)  # Keeps only 'YYYY-MM-DD' from date
+datetime_str = Calcofi_cast['Date'] + ' ' + Calcofi_cast['Time'] 
+Calcofi_cast['datetime'] = pd.to_datetime(datetime_str, errors='coerce') #put date and time together to form datetime column
 
-#to match up the metadata (Calcofi_cast) with the bottle data (Calcofi_btl), match by cast since each cast is unique ( All CalCOFI casts ever conducted, consecutively numbered)
+#to match up the metadata (Calcofi_cast) with the bottle data (Calcofi_btl), match by cast since each cast is unique ( from website: All CalCOFI casts ever conducted, consecutively numbered)
 Calcofi_btl['lat'] = Calcofi_btl['Cst_Cnt'].map(Calcofi_cast.set_index('Cst_Cnt')['Lat_Dec']) # map a lat column to where Cst_counts match, lat will be a copy of Lat_Dec
 Calcofi_btl['lon'] = Calcofi_btl['Cst_Cnt'].map(Calcofi_cast.set_index('Cst_Cnt')['Lon_Dec']) #same for lon
 Calcofi_btl['datetime'] = Calcofi_btl['Cst_Cnt'].map(Calcofi_cast.set_index('Cst_Cnt')['datetime']) #same for datetime
 
-#for this algorithm, only want data from 2000 on and above 150 meters 
+#subset data from 2000 onward and above 150 meters 
 Calcofi_btl = Calcofi_btl[Calcofi_btl['datetime'] > pd.to_datetime('2000-01-01')]
 Calcofi_btl=Calcofi_btl.loc[Calcofi_btl['Depthm']<=150].reset_index(drop=True) 
 
@@ -40,9 +40,8 @@ Calcofi_btl = Calcofi_btl[Calcofi_btl['Chlqua'] != 9]
 Calcofi_btl = Calcofi_btl[Calcofi_btl['Chlqua'] != 8]
 
 #HPLC flag
-#based on this: https://calcofi.info/index.php/field-work/bottle-sampling 
-#and this: https://calcofi.org/sampling-info/methods/ I don't think CALCOFI uses hplc on their chl bottle data
-Calcofi_btl['HPLC'] = 1
+#based on this: https://calcofi.info/index.php/field-work/bottle-sampling and this: https://calcofi.org/sampling-info/methods/  CALCOFI did not use hplc on their chl bottle data
+Calcofi_btl['HPLC'] = 1 
 
 #triplicate flag
 counts_series = Calcofi_btl[['Cst_Cnt','Depthm','datetime','lat','lon']].value_counts() #count how many unique datehour, lat, and lons there are
@@ -55,8 +54,11 @@ Calcofi_btl['triplicate'] = 1
 Calcofi_btl=Calcofi_btl.rename(columns={'Cst_Cnt':'cast', 'Sta_ID':'station','ChlorA':'chl','Depthm':'depth'})
 Calcofi_btl = Calcofi_btl[['datetime','lat', 'lon','chl', 'cast', 'station', 'depth','HPLC', 'triplicate']]
 
+#add additional metadata
 Calcofi_btl['identifier_product_doi'] = 'https://calcofi.org'
 Calcofi_btl['source'] = 'CalCOFI'
 Calcofi_btl=Calcofi_btl[['datetime', 'lat', 'lon', 'chl', 'depth','cast', 'station', 'HPLC','triplicate', 'identifier_product_doi', 'source']]
+
+
 Calcofi_btl.to_excel('calcofi_chl_qc.xlsx', index = False)
 
